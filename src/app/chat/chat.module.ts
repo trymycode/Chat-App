e08ec3a23bc3385bf6de8d810cea72c0033c6831
trymycode <imitasingha1994@gmail.com>
2019-01-10 20:27:34 +0530

    created featured modules and module components

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+
+@NgModule({
+  declarations: [ChatBoxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChatModule { }
