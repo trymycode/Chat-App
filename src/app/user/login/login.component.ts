93d70715e59f298cb86704a319d7970a2ed3ea76
trymycode <imitasingha1994@gmail.com>
2019-01-11 12:49:15 +0530

    modified login.component.ts

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
+import { AppComponent } from './../../app.component';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from './../../app.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public email: any;
+  public password: any;
 
-  constructor() { }
+  constructor( public appService: AppService, public router: Router ) { }
 
   ngOnInit() {
   }
+  public goToSignUp: any = () => {
+
+    this.router.navigate(['/sign-up']);
+
+  } // end goToSignUp
+
+  public signinFunction: any = () => {
+
+    if (!this.email) {
+      // this.toastr.warning('enter email')
+      alert("Enter email");
+
+    } else if (!this.password) {
+
+      // this.toastr.warning('enter password')
+      alert("Enter password");
+
+    } else {
+
+      let data = {
+        email: this.email,
+        password: this.password
+      }
+
+      this.appService.signinFunction(data)
+        .subscribe((apiResponse) => {
+
+          if (apiResponse.status === 200) {
+            console.log(apiResponse)
+
+            //  Cookie.set('authtoken', apiResponse.data.authToken);
+            
+            //  Cookie.set('receiverId', apiResponse.data.userDetails.userId);
+            
+            //  Cookie.set('receiverName', apiResponse.data.userDetails.firstName + ' ' + apiResponse.data.userDetails.lastName);
+           
+            //  this.appService.setUserInfoInLocalStorage(apiResponse.data.userDetails)
+            alert("Logged in successfully");
+             this.router.navigate(['/chat']);
+
+          } else {
+
+            // this.toastr.error(apiResponse.message)
+            alert("ERROR"+ apiResponse.message);
+
+          }
+
+        }, (err) => {
+          // this.toastr.error('some error occured')
+          alert("Some error occured!");
+        });
+
+    } // end condition
+
+  } // end signinFunction
+
+  
 
 }
